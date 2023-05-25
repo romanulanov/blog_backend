049bf3fdf7179654e637d032fd69160a6dbc406e
pomsk10 <turnsourcream@gmail.com>
2023-05-25 10:28:47 +0400

    render posts on index page

--- sensive_blog/settings.py
+++ sensive_blog/settings.py
@@ -90,9 +90,10 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = os.getenv('STATIC_URL', 'static/img/')
+STATIC_URL = os.getenv('STATIC_URL', 'static')
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static/img"), )
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), 
+                    )
 
 MEDIA_ROOT = os.getenv("MEDIA_ROOT", os.path.join(BASE_DIR, 'media'))
 
