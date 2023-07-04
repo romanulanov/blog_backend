15fce81a606d2c6e4c85c3363eb36ad9d8277bfb
pomsk10 <turnsourcream@gmail.com>
2023-07-04 16:33:44 +0400

    исправил битую ссылку; добавил подпись; исправил баг; добавил базовый шаблон

--- sensive_blog/settings.py
+++ sensive_blog/settings.py
@@ -90,9 +90,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = os.getenv('STATIC_URL', 'static')
+STATIC_URL = os.getenv('STATIC_URL', 'static/')
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), 
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static/"), 
                     )
 
 MEDIA_ROOT = os.getenv("MEDIA_ROOT", os.path.join(BASE_DIR, 'media'))
